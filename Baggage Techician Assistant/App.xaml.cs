7ff838ade3045a45d355893d6ef748e77ecc41e4
Creswell Gould <ccgould@yahoo.com>
2022-11-08 20:35:05 -0500

    Added .Net Maui

---
+++ Baggage Techician Assistant/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Baggage_Techician_Assistant;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Baggage Techician Assistant/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Baggage_Techician_Assistant;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
